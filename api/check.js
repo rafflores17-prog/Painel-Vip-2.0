98b804880260004df521a1065614d614821e3844
rafflores17-prog <rafflores17@gmail.com>
2026-04-05 01:39:31 -0300

    Update check.js

--- api/check.js
+++ api/check.js
@@ -11,18 +11,23 @@ export default async function handler(req, res) {
         const start = Date.now();
 
         const response = await fetch(url);
-
         const tempo = Date.now() - start;
+
+        const data = await response.json();
+
+        let statusReal = data?.user_info?.status || "Desconhecido";
 
         return res.status(200).json({
             online: response.ok,
-            tempo
+            tempo,
+            statusReal
         });
 
     } catch (e) {
         return res.status(200).json({
             online: false,
-            tempo: null
+            tempo: null,
+            statusReal: "Erro"
         });
     }
 }
